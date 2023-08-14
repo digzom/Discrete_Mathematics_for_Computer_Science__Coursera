4fe166067adfdde266317cd702774d57819be7da
dicksonmelo1 <dicksonmelo@gmail.com>
2023-08-14 05:25:27 -0300

    add comment

--- computer_search/queens_problem/queen_backtracking.py
+++ computer_search/queens_problem/queen_backtracking.py
@@ -1,9 +1,10 @@
-def can_be_extended(perm):
+from typing import List
+
+def can_be_extended(perm: List) -> bool:
     i = len(perm) - 1
     return all(i - j != abs(perm[i] - perm[j]) for j in range(i))
 
-
-def extend(perm, n):
+def extend(perm: List[int], n: int):
     if len(perm) == n:
         print(perm)
         exit()
@@ -14,4 +15,6 @@ def extend(perm, n):
                 extend(perm + [k], n)
 
 
+# I just need to pass the size of the board (in this case, is a 3x3 board) because
+# our intent is to make a permutation thought all cells
 extend(perm=[], n=24)

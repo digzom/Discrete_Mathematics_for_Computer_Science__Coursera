ec6e131df749c618da60574bbf07ba43cb107b95
dicksonmelo1 <dicksonmelo@gmail.com>
2023-08-16 22:21:30 -0300

    add a counter

--- computer_search/queens_problem/queen_backtracking.py
+++ computer_search/queens_problem/queen_backtracking.py
@@ -1,20 +1,28 @@
 from typing import List
 
-def can_be_extended(perm: List) -> bool:
+count = 0
+
+def can_be_extended(perm):
     i = len(perm) - 1
     return all(i - j != abs(perm[i] - perm[j]) for j in range(i))
 
-def extend(perm: List[int], n: int):
+def extend(perm, n):
+    global count
     if len(perm) == n:
         print(perm)
-        exit()
+        count += 1
 
     for k in range(n):
         if k not in perm:
             if can_be_extended(perm + [k]):
                 extend(perm + [k], n)
+                
+                
+                
 
 
 # I just need to pass the size of the board (in this case, is a 3x3 board) because
 # our intent is to make a permutation thought all cells
-extend(perm=[], n=24)
+extend(perm=[], n=8)
+print(count)
+

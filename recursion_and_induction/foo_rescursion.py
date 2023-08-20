df226e426afc494045ae1d6aa1d14e9da847e78a
digzom <dicksonmelo@gmail.com>
2023-08-20 05:56:02 -0300

    more stuff

--- recursion_and_induction/foo_rescursion.py
+++ recursion_and_induction/foo_rescursion.py
@@ -1,8 +1,8 @@
-def foo(n):
-  assert n >= 0
-  if n == 0:
-    return 0
+def foo(n): # 4 -> 3 -> 2 -> 1 -> 
+  assert n >= 0 # true -> true -> true -> true -> true
+  if n == 0: # false -> false -> false -> true
+    return 0 # jump -> jump -> jump -> 0
   else:
-    return foo(n - 1) + 2
+    return foo(n - 1) + 2 # 6 + 2 = 8 -> 4 + 2 = 6 -> 2 + 2 = 4 
 
 print(foo(2))

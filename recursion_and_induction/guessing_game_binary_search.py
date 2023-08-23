caad5c3b440e1a9b72c22ca097fec32e1c20dbae
digzom <dicksonmelo@gmail.com>
2023-08-23 04:43:12 -0300

    chore: book example about binary search with code guessing game

---
+++ recursion_and_induction/guessing_game_binary_search.py
@@ -0,0 +1,26 @@
+def query(y: int):
+  x = 5
+  if x == y:
+    return 'equal'
+  elif x < y:
+    return 'smaller'
+  else:
+    return 'greater'
+
+
+def guess(lower: int, upper: int):
+  middle = (lower + upper) // 2
+  answer = query(middle)
+
+  print(f'Is x = {middle}? It is {answer}')
+
+  if answer == 'equal':
+    return
+  elif answer == 'smaller':
+    guess(lower, middle - 1)
+  else: 
+    assert answer == 'greater'
+    guess(middle + 1, upper)
+
+
+guess(1, 5)

bb4be36e6d9471735a4459c4834ab33b10f9a638
digzom <dicksonmelo@gmail.com>
2023-08-30 19:09:57 -0300

    chore: add logic module'

---
+++ logic/prime_proof.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    assert n > 0
+    if n == 1:
+        return False
+
+    for d in range(2, n):
+        if n % d == 0:
+            return False
+
+    return True
+
+
+for n in range(1, 100):
+    if not is_prime(n ** 2 + n + 41):
+      print(n)
+      exit()
